f6ef70c61e52b719f26791ac3096e8d596115bcb
Austin Roberts <austin.roberts@rivet.cloud>
2022-04-29 10:34:15 -0500

    Gofmt websockets.go

--- transports/websockets.go
+++ transports/websockets.go
@@ -1,33 +1,32 @@
 package transports
 
-
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"math/rand"
-	"time"
-	"context"
 	"net/http"
-	"encoding/json"
+	"time"
 
-	"github.com/openrelayxyz/cardinal-rpc"
 	"github.com/gorilla/websocket"
 	log "github.com/inconshreveable/log15"
+	"github.com/openrelayxyz/cardinal-rpc"
 )
 
 type wsTransport struct {
-  port int64
-  semaphore chan struct{}
-  s *http.Server
-  running bool
-  registry rpc.Registry
+	port      int64
+	semaphore chan struct{}
+	s         *http.Server
+	running   bool
+	registry  rpc.Registry
 }
 
 func NewWSTransport(port int64, semaphore chan struct{}, registry rpc.Registry) Transport {
-  return &wsTransport{
-    port: port,
-    semaphore: semaphore,
-    registry: registry,
-  }
+	return &wsTransport{
+		port:      port,
+		semaphore: semaphore,
+		registry:  registry,
+	}
 }
 
 var upgrader = websocket.Upgrader{} // use default options
@@ -40,46 +39,48 @@ func (ws *wsTransport) echo(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 	for {
-	call := &rpc.Call{}
-  mt, body, err := c.ReadMessage()
-  if err != nil { return }
-  if err := json.Unmarshal(body, call); err == nil {
-    response := ws.handleSingle(r.Context(), call)
-		r, _ := json.Marshal(response)
-		c.WriteMessage(mt, r)
-		continue
-  }
-  calls := []rpc.Call{}
-  if err := json.Unmarshal(body, &calls); err == nil {
-    response := ws.handleBatch(r.Context(), calls)
-		for _, item := range response {
-			r, _ := json.Marshal(item)
-			c.WriteMessage(mt, r)
+		call := &rpc.Call{}
+		mt, body, err := c.ReadMessage()
+		if err != nil {
+			return
 		}
-  } else {
-    handleError(w, err)
-  }
-}
+		if err := json.Unmarshal(body, call); err == nil {
+			response := ws.handleSingle(r.Context(), call)
+			r, _ := json.Marshal(response)
+			c.WriteMessage(mt, r)
+			continue
+		}
+		calls := []rpc.Call{}
+		if err := json.Unmarshal(body, &calls); err == nil {
+			response := ws.handleBatch(r.Context(), calls)
+			for _, item := range response {
+				r, _ := json.Marshal(item)
+				c.WriteMessage(mt, r)
+			}
+		} else {
+			handleError(w, err)
+		}
+	}
 }
 
 func (ws *wsTransport) Start(failure chan error) error {
-  if ws.s != nil {
-    return fmt.Errorf("wsTransport already started")
-  }
-  rand.Seed(time.Now().UnixNano())
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", ws.echo)
-  ws.s = &http.Server{
-    Addr: fmt.Sprintf(":%v", ws.port),
-    Handler: mux,
-    ReadHeaderTimeout: 5 * time.Second,
-    IdleTimeout: 120 * time.Second,
-    MaxHeaderBytes: 1 << 20,
-  }
-  go func() { failure <- ws.s.ListenAndServe() }()
-  log.Info("Running web socket transport", "port:", ws.port)
-  ws.running = true
-  return nil
+	if ws.s != nil {
+		return fmt.Errorf("wsTransport already started")
+	}
+	rand.Seed(time.Now().UnixNano())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ws.echo)
+	ws.s = &http.Server{
+		Addr:              fmt.Sprintf(":%v", ws.port),
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+	}
+	go func() { failure <- ws.s.ListenAndServe() }()
+	log.Info("Running web socket transport", "port:", ws.port)
+	ws.running = true
+	return nil
 }
 
 func (ws *wsTransport) Stop() error {
@@ -88,28 +89,28 @@ func (ws *wsTransport) Stop() error {
 }
 
 func (ws *wsTransport) handleSingle(ctx context.Context, call *rpc.Call) *rpc.Response {
-  ws.semaphore <- struct{}{}
-  start := time.Now()
-  result, err, meta := ws.registry.Call(ctx, call.Method, call.Params)
-  <-ws.semaphore
-  meta.Duration = time.Since(start)
-  response := &rpc.Response{
-    Version: "2.0",
-    ID: call.ID,
-    Meta: meta,
-  }
-  if err == nil {
-    response.Result = result
-  } else {
-    response.Error = err
-  }
-  return response
+	ws.semaphore <- struct{}{}
+	start := time.Now()
+	result, err, meta := ws.registry.Call(ctx, call.Method, call.Params)
+	<-ws.semaphore
+	meta.Duration = time.Since(start)
+	response := &rpc.Response{
+		Version: "2.0",
+		ID:      call.ID,
+		Meta:    meta,
+	}
+	if err == nil {
+		response.Result = result
+	} else {
+		response.Error = err
+	}
+	return response
 }
 
 func (ws *wsTransport) handleBatch(ctx context.Context, calls []rpc.Call) []rpc.Response {
-  results := make([]rpc.Response, len(calls))
-  for i, call := range calls {
-    results[i] = *ws.handleSingle(ctx, &call)
-  }
-  return results
+	results := make([]rpc.Response, len(calls))
+	for i, call := range calls {
+		results[i] = *ws.handleSingle(ctx, &call)
+	}
+	return results
 }
